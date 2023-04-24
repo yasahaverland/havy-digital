3d29f4451c1081cfe6087a56bc22d54c5816c346
yasahaverland <yasahaverland@icloud.com>
2023-04-24 03:15:53 -0400

    modelo 3D adicionado - bug

---
+++ src/components/canvas/Graphs.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+
+const Graphs = () => {
+
+  const graph = useGLTF("../../desktop_pc/scene.gltf");
+  console.log(graph);
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <primitive 
+        object={graph.scene}
+        scale={0.75}
+        position={[0,0,0]} 
+      />
+    </mesh>
+  )
+}
+
+
+const GraphCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }} 
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          />
+          <Graphs />
+      </Suspense>
+      <Preload all />
+
+    </Canvas>
+    
+  )
+}
+
+export default GraphCanvas;
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,6 @@
 import PiggyCanvas from "./Piggy";
 import EarthCanvas from "./Earth";
 import MoneyCanvas from "./Money";
-import GraphCanvas from "./Graph";
+import GraphCanvas from "./Graphs";
 
 export { PiggyCanvas, EarthCanvas, MoneyCanvas, GraphCanvas };
