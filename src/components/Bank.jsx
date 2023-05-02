cf178cf5bbc634c707f056c47ae4e9cb977440d1
yasahaverland <yasahaverland@icloud.com>
2023-05-02 00:28:42 -0400

    making it more responsive

--- src/components/Bank.jsx
+++ src/components/Bank.jsx
@@ -21,9 +21,9 @@ const Bank = () => {
       </div>
       <GraphsCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-25 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[30px] h-[45px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2'>
             <motion.dev
               animate={{y: [0, 24, 0]}}
               transition={{
--- src/components/canvas/Graphs.jsx
+++ src/components/canvas/Graphs.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import React, { Suspense, useEffect, useState } from "react";
@@ -7,7 +9,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 
-const Graphs = () => {
+const Graphs = ({ isMobile }) => {
 
   const graph = useGLTF("/statistic_charts_with_arrow/scene.gltf");
   // console.log(graph);
@@ -26,8 +28,8 @@ const Graphs = () => {
       />
       <primitive 
         object={graph.scene}
-        scale={1}
-        position={[0.5,-3,0]} 
+        scale={isMobile ? 0.5 : 1}
+        position={ isMobile ? [0, -4, 0] : [0.5,-3,0]} 
       />
     </mesh>
   )
@@ -35,6 +37,27 @@ const Graphs = () => {
 
 
 const GraphsCanvas = () => {
+  // modificar o modelo 3d de acordo com o tamanho da tela para melhor adaptaçao entre diferentes aparelhos.
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    // callback function para identificar as mudanças no media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    // remove o event listener quando o component esta unmounted (fim do component cycle)
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <Canvas height="100vh" width="100vw" frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
 
@@ -44,7 +67,7 @@ const GraphsCanvas = () => {
           maxPolarAngle={Math.PI / 2} // da um limite pra "camera" não conseguir olhar completamente para baixo
           minPolarAngle={0} // limita a "camera" de olhar completamente pra cima
           />
-          <Graphs />
+          <Graphs isMobile={ isMobile } />
       </Suspense>
       <Preload all />
     </Canvas>
